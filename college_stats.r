e11a9022b0b68e0dd219479384bbb961522ba52a
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-05-19 21:33:36 +0300

    add first stage of hypothesis verification

--- college_stats.r
+++ college_stats.r
@@ -1,7 +1,11 @@
 RESOURCE_PATH = "resources"
 
-save_plot <- function(filename, plot_func, ...) {
-  filepath = file.path(RESOURCE_PATH, paste(filename, ".png", sep = ""))
+filepath_png <- function(name) {
+  return(file.path(RESOURCE_PATH, paste(name, ".png", sep = "")))
+}
+
+save_plot <- function(name, plot_func, ...) {
+  filepath = filepath_png(name)
   png(filepath)
   plot_func(...)
   dev.off()
@@ -54,7 +58,6 @@ men_write = df[df$genre == "male",]
 women_write = df[df$genre == "female",]
 
 # compute diff
-# https://link.springer.com/article/10.3758/BF03210951
 diff <- mean(df$write) - women_write$write
 diff
 
@@ -71,7 +74,6 @@ leveneTest(write ~ genre, data=df)
 bartlett.test(formula, df)
 
 # use Welch test regardless of variance analysis
-# https://bpspsychub.onlinelibrary.wiley.com/doi/abs/10.1348/000711004849222
 t.test(men_write$write, women_write$write)
 t.test(men_write$write, women_write$write, alternative = "less")
 
@@ -99,14 +101,19 @@ kruskal.test(formula, data=df)
 # show boxplots
 
 
-# =====erotima c =====
+# ===== erotima c =====
 math_model = lm(math ~ .,df)
 summary(math_model)
 
 # check for outliers
+
+# manually saved because of multiple functioned used for one plot
+filepath = filepath_png("lm_math_residual_plot")
+png(filepath)
 plot(math_model$fit, rstandard(math_model))
 abline(h=-1.96, col='red', lwd=2, lty=2)
 abline(h=1.96, col='red', lwd=2, lty=2)
+dev.off()
 # no outliers found
 
 # check for normality
@@ -119,9 +126,9 @@ quantcut <- function(x, digits=6) {
 
 qfits <- quantcut(math_model$fit)
 leveneTest(rstandard(math_model), qfits)
-boxplot(rstandard(math_model)~qfits, boxcol=0,boxfill=3, medlwd=2, 
+save_plot("lm_math_residual_boxplot", boxplot, rstandard(math_model)~qfits, boxcol=0,boxfill=3, medlwd=2, 
         medcol="white", cex=1.5, pch=16, col='blue', xlab = "Quantiles", 
-        ylab="Standardized residuals")
+        ylab="Standardized residuals", names=c("Q1","Q2","Q3","Q4"))
 
 # check for autocorrelation
 durbinWatsonTest(math_model)
@@ -145,9 +152,12 @@ socst_model_orig = lm(socst ~ . - id,df)
 summary(socst_model)
 
 # check for outliers
+filepath = filepath_png("lm_socst_residual_plot")
+png(filepath)
 plot(socst_model_orig$fit, rstandard(socst_model_orig))
 abline(h=-1.96, col='red', lwd=2, lty=2)
 abline(h=1.96, col='red', lwd=2, lty=2)
+dev.off()
 #identify(socst_model_orig$fit, rstandard(socst_model_orig),n=1)
 # outlier 163 found
 
@@ -161,9 +171,10 @@ shapiro.test(rstandard(socst_model))
 
 qfits <- quantcut(socst_model$fit)
 leveneTest(rstandard(socst_model), qfits)
-boxplot(rstandard(socst_model)~qfits, boxcol=0,boxfill=3, medlwd=2, 
-        medcol="white", cex=1.5, pch=16, col='blue', xlab = "Quantiles", 
-        ylab="Standardized residuals")
+save_plot("lm_socst_residual_boxplot", boxplot, rstandard(socst_model)~qfits, 
+          boxcol=0,boxfill=3, medlwd=2, medcol="white", cex=1.5, pch=16, 
+          col='blue', xlab = "Quantiles", ylab="Standardized residuals", 
+          names=c("Q1","Q2","Q3","Q4"))
 
 # check for autocorrelation
 durbinWatsonTest(socst_model)
@@ -172,11 +183,12 @@ durbinWatsonTest(socst_model, method="normal")
 library(MASS)
 fullModel = lm(socst ~ . - id, data = df[-163,]) 
 nullModel = lm(socst ~ 1, data = df[-163,]) 
-optimal_socst_model = stepAIC(socst_model,
-                direction = 'both', 
-                scope = list(upper = fullModel, 
-                             lower = nullModel), 
-                trace = 0) # do not show the step-by-step process of model selection
+optimal_socst_model = stepAIC(
+                  socst_model,
+                  direction = 'both', 
+                  scope = list(upper = fullModel, 
+                               lower = nullModel), 
+                  trace = 0) # do not show the step-by-step process of model selection
 summary(optimal_socst_model)
 
 # Do we need to re-check preconditions here?
@@ -184,4 +196,51 @@ summary(optimal_socst_model)
 stargazer(optimal_socst_model, type="latex", 
           title="Linear regression model predicting social study test scores, taking into 
           account other test scores.", ci=T, label="tab::lm_socst_peeking", df=T,
-          out="lm_socst_peeking.tex")
+          out="lm_socst_peeking.tex", report=('vc*p'))
+
+
+# ===== erotima d =====
+
+no_peek_mmodel = lm(math ~ genre + race + schtyp + prog + socst, data=df)
+summary(no_peek_mmodel)
+
+# manually saved because of multiple functioned used for one plot
+filepath = filepath_png("lm_math_nopeek_residual_plot")
+png(filepath)
+plot(no_peek_mmodel$fit, rstandard(no_peek_mmodel))
+abline(h=-1.96, col='red', lwd=2, lty=2)
+abline(h=1.96, col='red', lwd=2, lty=2)
+#identify(no_peek_mmodel$fit, rstandard(no_peek_mmodel),n=3)
+# possible outliers: 22, 37, 194
+dev.off()
+
+# check for normality
+shapiro.test(rstandard(no_peek_mmodel))
+
+qfits <- quantcut(no_peek_mmodel$fit)
+leveneTest(rstandard(no_peek_mmodel), qfits)
+save_plot("lm_mmath_nopeek_residual_boxplot", boxplot, rstandard(no_peek_mmodel)~qfits, 
+          boxcol=0,boxfill=3, medlwd=2, medcol="white", cex=1.5, pch=16, 
+          col='blue', xlab = "Quantiles", ylab="Standardized residuals", 
+          names=c("Q1","Q2","Q3","Q4"), report=('vc*p'))
+
+# check for autocorrelation
+durbinWatsonTest(no_peek_mmodel)
+durbinWatsonTest(no_peek_mmodel, method="normal")
+
+fullModel = lm(math ~ . - id - write, data = df) 
+nullModel = lm(math ~ 1, data = df) 
+optimal_nopeek_mmodel = stepAIC(no_peek_mmodel,
+                              direction = 'backward', 
+                              scope = list(upper = fullModel, 
+                                           lower = nullModel), 
+                              trace = 0)
+summary(optimal_nopeek_mmodel)
+
+# Do we need to re-check preconditions here?
+
+stargazer(optimal_nopeek_mmodel, type="latex", 
+          title="Linear regression model predicting social study test scores, 
+          without relying on the writing tests", ci=T, label="tab::lm_math_nopeeking", df=T,
+          out="lm_math_nopeeking.tex", report=('vc*p'))
+
